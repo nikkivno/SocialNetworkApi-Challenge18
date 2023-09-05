3641d207d335f2e3330438b7cb4bb7eada2ce0e5
nikkivno <nikkivigneault@gmail.com>
2023-09-05 13:31:20 -0300

    updated getThoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,17 +6,14 @@ const thoughtCount = async() => {
     return numberofThoughts;
 }
 
-// execute aggregate
 
 module.exports = {
     async getThoughts(req,res) {
         try {
-            const throughts = await Thought.find();
-            const thoughtobj = {
-                thoughts, 
-                numberofThoughts = await numberofThoughts(),
-            };
-            return res.json(thoughtobj);
+            const thoughts = await Thought.find()
+            .sort({createdAt: -1});
+            
+            return res.json(thoughts);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
@@ -59,6 +56,44 @@ module.exports = {
             if (!thought) {
                 return res.status(404).json({message: 'No thought with this ID'})
             }
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+
+        try {
+            const user = await User.findOneAndUpdate(
+                { users: req.params.userId },
+                { $pull: { users: req.params.userId } },
+                { new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({message: 'No user with this ID'})
+            }
+            res.json({message: 'Thought successfully deleted'})
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async addThought(req,res) {
+        try {
+            console.log('You are adding a Thought');
+            console.log(req.body);
+            const thought = await Thought.create(req.body);
+            console.log(thought);
+
+            // TODO: Update the user's thoughts by adding the thought ID to it
+            // The user whose id (findOneAndUpdate for the id that matches req.body.userId)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+        try {
+
+        } catch (err) {
+
         }
     },
 
