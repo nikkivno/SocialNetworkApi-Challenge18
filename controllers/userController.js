3b9034090b2e10f84289b0c6f929ea7e891ab0cd
nikkivno <nikkivigneault@gmail.com>
2023-09-05 13:30:00 -0300

    added in add and remove thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,4 +65,43 @@ async updateUser(req,res) {
         res.status(500).json(err);
     }
 },
+
+async addThought(req,res) {
+    try {
+        console.log('You are adding a thought!');
+        console.log(req.body);
+
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { thoughts: req.body } },
+            { runValidators: true, new: true } 
+        );
+
+        if (!user) {
+            return res.status(404).json({message: 'No user found with this ID'})
+        }
+
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+async removeThought(req,res) {
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { thought: { thoughtId: req.params.thoughtId } } },
+            { runValidators: true, new: true }
+        );
+
+        if (!user) {
+            return res.status(404).json({message: 'No user found with that ID'})
+        }
+
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
 };
